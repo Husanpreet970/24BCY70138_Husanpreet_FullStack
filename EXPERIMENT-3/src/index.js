0d91a1a7efeb7c4c068551d043ed0bae870ea57b
Husanpreet Kaur <husankajji@gmail.com>
2026-08-22 21:45:46 +0530

    Set up index.js for React Router and Axios
    Initialize React application with routing and API integration.

---
+++ EXPERIMENT-3/src/index.js
@@ -0,0 +1,16 @@
+//Experiment 3 — React Router and Axios API Integration
+//Problem Statement
+//Develop a React application that demonstrates client-side routing, dynamic URL parameters, query parameters, Axios API communication, Axios interceptors, custom React Hooks, useMemo(), and passing data between parent and child components using props.
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
